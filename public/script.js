6ad119dc566e263c1da152e082182c54924e3688
gustavopeq <gustavo_pequeno2@hotmail.com>
2020-03-16 15:00:13 -0700

    Github automatic deployment to Heroku

--- public/script.js
+++ public/script.js
@@ -1,55 +1,35 @@
-$(document).ready(function(){
-    $("#input_open").change(onOpenChange);
+fetch("./pwnedtrim.json")
+.then(function(resp)
+{
+    return resp.json();
 })
+.then(function(data)
+{
+    let corrupteds = [];
 
-function onOpenChange(e) {
-    var filname = $("#input_open").val();
-    var fileContent = getTxt();
-    var jsonData = JSON.parse(fileContent);
-}
-getTxt = function (){
-
-  $.ajax({
-  url:'text.json',
-  success: function (data){
-      fileContent =data;           
-      return data 
-  }
-  });
-}
-
-// fetch("./pwnedtrim.json")
-// .then(function(resp)
-// {
-//     return resp.json();
-// })
-// .then(function(data)
-// {
-//     let corrupteds = [];
-
-//     for (let i = 0; i < data.length; i++) {
-//         const elementData = data[i];
-//         for (let j = 0; j < elementData.Breaches.length; j++) {
-//             const elementBreaches = elementData.Breaches[j];
-//             let canBreak = false;
-//             for (let k = 0; k < elementBreaches.DataClasses.length; k++) {
-//                 const elementDataClasses = elementBreaches.DataClasses[k];
+    for (let i = 0; i < data.length; i++) {
+        const elementData = data[i];
+        for (let j = 0; j < elementData.Breaches.length; j++) {
+            const elementBreaches = elementData.Breaches[j];
+            let canBreak = false;
+            for (let k = 0; k < elementBreaches.DataClasses.length; k++) {
+                const elementDataClasses = elementBreaches.DataClasses[k];
                 
-//                 if(elementDataClasses == 'Passwords')
-//                 {
-//                     corrupteds.push(elementData);
-//                     canBreak = true;
-//                 }
-//             }
-//             if(canBreak)
-//             {
-//                 break;
-//             }
-//         }
-//     }
+                if(elementDataClasses == 'Passwords')
+                {
+                    corrupteds.push(elementData);
+                    canBreak = true;
+                }
+            }
+            if(canBreak)
+            {
+                break;
+            }
+        }
+    }
 
-//     corrupteds.forEach(element => 
-//         {
-//             console.log(element.Alias);
-//         });
-// });
+    corrupteds.forEach(element => 
+        {
+            console.log(element.Alias);
+        });
+});
